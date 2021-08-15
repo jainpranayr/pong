0b8221cbead8e3adae94968c22f2face12c00de9
Pranay Jain <jpranay688@gmail.com>
2021-08-15 23:28:39 +0530

    add class ball

--- index.js
+++ index.js
@@ -12,11 +12,20 @@ class Rect {
   }
 }
 
+class Ball extends Rect {
+  constructor() {
+    super(10, 10);
+    this.vel = new Vector();
+  }
+}
+
 const canvas = document.querySelector("#pong");
 const context = canvas.getContext("2d");
 
+const ball = new Ball();
+
 context.fillStyle = "#000";
-context.fillRect(0, 0, canvas.clientWidth, canvas.height);
+context.fillRect(0, 0, canvas.width, canvas.height);
 
 context.fillStyle = "#fff";
-context.fillRect(0, 0, 10, 10);
+context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
