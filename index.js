7884c398cd02151c621daed71c0c9601e5a34df0
Pranay Jain <jpranay688@gmail.com>
2021-08-16 01:17:52 +0530

    add boundary detection

--- index.js
+++ index.js
@@ -41,6 +41,13 @@ function update(dt) {
   ball.pos.x += ball.vel.x * dt;
   ball.pos.y += ball.vel.y * dt;
 
+  if (ball.pos.x < 0 || ball.pos.x > canvas.width) {
+    ball.vel.x = -ball.vel.x;
+  }
+  if (ball.pos.y < 0 || ball.pos.y > canvas.height) {
+    ball.vel.y = -ball.vel.y;
+  }
+
   context.fillStyle = "#000";
   context.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -48,10 +55,10 @@ function update(dt) {
   context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
 }
 
+callback();
+
 context.fillStyle = "#000";
 context.fillRect(0, 0, canvas.width, canvas.height);
 
 context.fillStyle = "#fff";
 context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
-
-callback();
