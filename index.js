9b206440125864d8a2b24af1391f5fbea32be074
Pranay Jain <jpranay688@gmail.com>
2021-08-15 23:18:48 +0530

    initialise canvas

---
+++ index.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector("#pong");
+const context = canvas.getContext("2d");
+
+context.fillStyle = "#000";
+context.fillRect(0, 0, canvas.clientWidth, canvas.height);
+
+context.fillStyle = "#fff";
+context.fillRect(0, 0, 10, 10);
