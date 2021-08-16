18b1890f2dbf8914e1b1fabd85c581c8d20bc12b
Pranay Jain <jpranay688@gmail.com>
2021-08-17 01:54:17 +0530

    fix move cursor to play

--- index.js
+++ index.js
@@ -197,8 +197,5 @@ const pong = new Pong(canvas);
 canvas.addEventListener("mousemove", (event) => {
   const scale = event.offsetY / event.target.getBoundingClientRect().height;
   pong.players[0].pos.y = canvas.height * scale;
-});
-
-canvas.addEventListener("click", (event) => {
   pong.start();
 });
